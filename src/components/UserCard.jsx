5baef6963cddab08653c58bf7377fa518d24120d
Asmaa Abdi <asmaaabdi03@gmail.com>
2024-11-14 23:59:15 +0300

    lab

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+
+import { Link } from "react-router-dom";
+
+function MovieCard({ movie }) {
+  return (
+    <div>
+      <h2>{movie.title}</h2>
+      <Link to={`/movie/${movie.id}`}>View Info</Link>
+    </div>
+  );
+}
+
+export default MovieCard;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,21 @@
+
+import { useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </main>
+    </>
+  );
+}
+
+export default ErrorPage;
